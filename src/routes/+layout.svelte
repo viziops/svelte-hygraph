b073f8cb2ed68321717683d94cc149f55fe75f98
Edwin <dev@viziops.com>
2023-01-11 14:25:28 +0200

    darkmode svg icon

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -26,7 +26,7 @@
 				viewBox="0 0 24 24"
 				stroke-width="1.5"
 				stroke="currentColor"
-				class="stroke-cyan-500 hover:stroke-cyan-700""
+				class="w-6 h-6"
 			>
 				<path
 					stroke-linecap="round"
