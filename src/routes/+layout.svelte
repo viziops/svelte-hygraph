cb3d8d7fd79f13f2bd3b4f407a34e3a57b9f81ae
Edwin <dev@viziops.com>
2023-01-11 14:20:30 +0200

    Darkmode added

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -9,18 +9,34 @@
 		FooterLink,
 		FooterLinkGroup
 	} from 'flowbite-svelte';
+	import { DarkMode } from 'flowbite-svelte';
+
+	let btnClass =
+		'text-gray-500 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-700 rounded-lg text-xl p-2';
 </script>
+
+<DarkMode class="text-lg absolute top-2 right-4" />
 
 <div class="container mx-auto px-4">
 	<Navbar let:hidden let:toggle>
 		<NavBrand href="/">
-			<img
-				src="https://flowbite.com/docs/images/logo.svg"
-				class="mr-3 h-6 sm:h-9"
-				alt="Flowbite Logo"
-			/>
+			<svg
+				xmlns="http://www.w3.org/2000/svg"
+				fill="none"
+				viewBox="0 0 24 24"
+				stroke-width="1.5"
+				stroke="currentColor"
+				class="stroke-cyan-500 hover:stroke-cyan-700""
+			>
+				<path
+					stroke-linecap="round"
+					stroke-linejoin="round"
+					d="M2.036 12.322a1.012 1.012 0 010-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178z"
+				/>
+				<path stroke-linecap="round" stroke-linejoin="round" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
+			</svg>
 			<span class="self-center whitespace-nowrap text-xl font-semibold dark:text-white">
-				Flowbite
+				Feed the Search
 			</span>
 		</NavBrand>
 		<NavHamburger on:click={toggle} />
