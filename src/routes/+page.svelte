a2a4c2c4e557542bcf8d0ee8cab23fad0248a62e
Edwin <dev@viziops.com>
2023-01-11 21:05:15 +0200

    hero and cards added

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,205 @@
 <script>
 	import { Tabs, TabItem } from 'flowbite-svelte';
+	import { Card } from 'flowbite-svelte';
 </script>
 
+<div class="relative px-2 lg:px-4">
+	<div class="mx-auto max-w-3xl pt-10 pb-20 sm:pt-36 sm:pb-36">
+		<div>
+			<div class="hidden sm:mb-8 sm:flex sm:justify-center">
+				<div
+					class="relative overflow-hidden rounded-full py-1.5 px-4 text-sm leading-6 ring-1 ring-gray-900/10 hover:ring-gray-900/20"
+				>
+					<span class="text-gray-600">
+						Announcing our next round of funding. <a href="#" class="font-semibold text-indigo-600"
+							><span class="absolute inset-0" aria-hidden="true" />Read more
+							<span aria-hidden="true">&rarr;</span></a
+						>
+					</span>
+				</div>
+			</div>
+			<div>
+				<h1 class="text-4xl font-bold tracking-tight sm:text-center sm:text-6xl">
+					Data to enrich your online business
+				</h1>
+				<p class="mt-6 text-lg leading-8 text-gray-600 sm:text-center">
+					Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo.
+					Elit sunt amet fugiat veniam occaecat fugiat aliqua.
+				</p>
+				<div class="mt-8 flex gap-x-4 sm:justify-center">
+					<a
+						href="#"
+						class="inline-block rounded-lg bg-indigo-600 px-4 py-1.5 text-base font-semibold leading-7 text-white shadow-sm ring-1 ring-indigo-600 hover:bg-indigo-700 hover:ring-indigo-700"
+					>
+						Get started
+						<span class="text-indigo-200" aria-hidden="true">&rarr;</span>
+					</a>
+					<a
+						href="#"
+						class="inline-block rounded-lg px-4 py-1.5 text-base font-semibold leading-7 text-gray-900 ring-1 ring-gray-900/10 hover:ring-gray-900/20"
+					>
+						Live demo
+						<span class="text-gray-400" aria-hidden="true">&rarr;</span>
+					</a>
+				</div>
+			</div>
+			<div
+				class="absolute inset-x-0 top-[calc(100%-13rem)] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[calc(100%-30rem)]"
+			>
+				<svg
+					class="relative left-[calc(50%+3rem)] h-[21.1875rem] max-w-none -translate-x-1/2 sm:left-[calc(50%+36rem)] sm:h-[42.375rem]"
+					viewBox="0 0 1155 678"
+					fill="none"
+					xmlns="http://www.w3.org/2000/svg"
+				>
+					<path
+						fill="url(#ecb5b0c9-546c-4772-8c71-4d3f06d544bc)"
+						fill-opacity=".3"
+						d="M317.219 518.975L203.852 678 0 438.341l317.219 80.634 204.172-286.402c1.307 132.337 45.083 346.658 209.733 145.248C936.936 126.058 882.053-94.234 1031.02 41.331c119.18 108.451 130.68 295.337 121.53 375.223L855 299l21.173 362.054-558.954-142.079z"
+					/>
+					<defs>
+						<linearGradient
+							id="ecb5b0c9-546c-4772-8c71-4d3f06d544bc"
+							x1="1155.49"
+							x2="-78.208"
+							y1=".177"
+							y2="474.645"
+							gradientUnits="userSpaceOnUse"
+						>
+							<stop stop-color="#9089FC" />
+							<stop offset="1" stop-color="#FF80B5" />
+						</linearGradient>
+					</defs>
+				</svg>
+			</div>
+		</div>
+	</div>
+</div>
+<div class="grid grid-cols-3 gap-4">
+	<div>
+		<Card>
+			<svg
+				xmlns="http://www.w3.org/2000/svg"
+				fill="none"
+				viewBox="0 0 24 24"
+				stroke-width="1.5"
+				stroke="currentColor"
+				class="w-12 h-12 mb-2text-gray-500 dark:text-gray-400 "
+				><path
+					stroke-linecap="round"
+					stroke-linejoin="round"
+					d="M21 11.25v8.25a1.5 1.5 0 01-1.5 1.5H5.25a1.5 1.5 0 01-1.5-1.5v-8.25M12 4.875A2.625 2.625 0 109.375 7.5H12m0-2.625V7.5m0-2.625A2.625 2.625 0 1114.625 7.5H12m0 0V21m-8.625-9.75h18c.621 0 1.125-.504 1.125-1.125v-1.5c0-.621-.504-1.125-1.125-1.125h-18c-.621 0-1.125.504-1.125 1.125v1.5c0 .621.504 1.125 1.125 1.125z"
+				/></svg
+			>
+			<a href="/">
+				<h5 class="mb-2 text-2xl font-semibold tracking-tight text-gray-900 dark:text-white">
+					Need a help in Claim?
+				</h5>
+			</a>
+			<p class="mb-3 font-normal text-gray-500 dark:text-gray-400">
+				Go to this step by step guideline process on how to certify for your weekly benefits:
+			</p>
+			<a href="/" class="inline-flex items-center text-blue-600 hover:underline">
+				See our guideline
+				<svg
+					xmlns="http://www.w3.org/2000/svg"
+					fill="none"
+					viewBox="0 0 24 24"
+					stroke-width="1.5"
+					stroke="currentColor"
+					class="w-5 h-5 ml-2"
+					><path
+						stroke-linecap="round"
+						stroke-linejoin="round"
+						d="M13.5 6H5.25A2.25 2.25 0 003 8.25v10.5A2.25 2.25 0 005.25 21h10.5A2.25 2.25 0 0018 18.75V10.5m-10.5 6L21 3m0 0h-5.25M21 3v5.25"
+					/></svg
+				>
+			</a>
+		</Card>
+	</div>
+	<div>
+		<Card>
+			<svg
+				xmlns="http://www.w3.org/2000/svg"
+				fill="none"
+				viewBox="0 0 24 24"
+				stroke-width="1.5"
+				stroke="currentColor"
+				class="w-12 h-12 mb-2text-gray-500 dark:text-gray-400 "
+				><path
+					stroke-linecap="round"
+					stroke-linejoin="round"
+					d="M21 11.25v8.25a1.5 1.5 0 01-1.5 1.5H5.25a1.5 1.5 0 01-1.5-1.5v-8.25M12 4.875A2.625 2.625 0 109.375 7.5H12m0-2.625V7.5m0-2.625A2.625 2.625 0 1114.625 7.5H12m0 0V21m-8.625-9.75h18c.621 0 1.125-.504 1.125-1.125v-1.5c0-.621-.504-1.125-1.125-1.125h-18c-.621 0-1.125.504-1.125 1.125v1.5c0 .621.504 1.125 1.125 1.125z"
+				/></svg
+			>
+			<a href="/">
+				<h5 class="mb-2 text-2xl font-semibold tracking-tight text-gray-900 dark:text-white">
+					Need a help in Claim?
+				</h5>
+			</a>
+			<p class="mb-3 font-normal text-gray-500 dark:text-gray-400">
+				Go to this step by step guideline process on how to certify for your weekly benefits:
+			</p>
+			<a href="/" class="inline-flex items-center text-blue-600 hover:underline">
+				See our guideline
+				<svg
+					xmlns="http://www.w3.org/2000/svg"
+					fill="none"
+					viewBox="0 0 24 24"
+					stroke-width="1.5"
+					stroke="currentColor"
+					class="w-5 h-5 ml-2"
+					><path
+						stroke-linecap="round"
+						stroke-linejoin="round"
+						d="M13.5 6H5.25A2.25 2.25 0 003 8.25v10.5A2.25 2.25 0 005.25 21h10.5A2.25 2.25 0 0018 18.75V10.5m-10.5 6L21 3m0 0h-5.25M21 3v5.25"
+					/></svg
+				>
+			</a>
+		</Card>
+	</div>
+	<div>
+		<Card>
+			<svg
+				xmlns="http://www.w3.org/2000/svg"
+				fill="none"
+				viewBox="0 0 24 24"
+				stroke-width="1.5"
+				stroke="currentColor"
+				class="w-12 h-12 mb-2text-gray-500 dark:text-gray-400 "
+				><path
+					stroke-linecap="round"
+					stroke-linejoin="round"
+					d="M21 11.25v8.25a1.5 1.5 0 01-1.5 1.5H5.25a1.5 1.5 0 01-1.5-1.5v-8.25M12 4.875A2.625 2.625 0 109.375 7.5H12m0-2.625V7.5m0-2.625A2.625 2.625 0 1114.625 7.5H12m0 0V21m-8.625-9.75h18c.621 0 1.125-.504 1.125-1.125v-1.5c0-.621-.504-1.125-1.125-1.125h-18c-.621 0-1.125.504-1.125 1.125v1.5c0 .621.504 1.125 1.125 1.125z"
+				/></svg
+			>
+			<a href="/">
+				<h5 class="mb-2 text-2xl font-semibold tracking-tight text-gray-900 dark:text-white">
+					Need a help in Claim?
+				</h5>
+			</a>
+			<p class="mb-3 font-normal text-gray-500 dark:text-gray-400">
+				Go to this step by step guideline process on how to certify for your weekly benefits:
+			</p>
+			<a href="/" class="inline-flex items-center text-blue-600 hover:underline">
+				See our guideline
+				<svg
+					xmlns="http://www.w3.org/2000/svg"
+					fill="none"
+					viewBox="0 0 24 24"
+					stroke-width="1.5"
+					stroke="currentColor"
+					class="w-5 h-5 ml-2"
+					><path
+						stroke-linecap="round"
+						stroke-linejoin="round"
+						d="M13.5 6H5.25A2.25 2.25 0 003 8.25v10.5A2.25 2.25 0 005.25 21h10.5A2.25 2.25 0 0018 18.75V10.5m-10.5 6L21 3m0 0h-5.25M21 3v5.25"
+					/></svg
+				>
+			</a>
+		</Card>
+	</div>
+</div>
 <Tabs style="underline">
 	<TabItem open title="Profile">
 		<p class="text-sm text-gray-500 dark:text-gray-400">
