bb253f072c2c8211fa868186f62e7c6cdb7da4e3
Edwin <dev@viziops.com>
2023-01-11 12:33:30 +0200

    Containerized

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,52 @@
 <script>
 	import '../app.css';
+	import { Navbar, NavBrand, NavLi, NavUl, NavHamburger } from 'flowbite-svelte';
+	import {
+		Footer,
+		FooterBrand,
+		FooterCopyright,
+		FooterIcon,
+		FooterLink,
+		FooterLinkGroup
+	} from 'flowbite-svelte';
 </script>
 
-<slot />
+<div class="container mx-auto px-4">
+	<Navbar let:hidden let:toggle>
+		<NavBrand href="/">
+			<img
+				src="https://flowbite.com/docs/images/logo.svg"
+				class="mr-3 h-6 sm:h-9"
+				alt="Flowbite Logo"
+			/>
+			<span class="self-center whitespace-nowrap text-xl font-semibold dark:text-white">
+				Flowbite
+			</span>
+		</NavBrand>
+		<NavHamburger on:click={toggle} />
+		<NavUl {hidden}>
+			<NavLi href="/" active={true}>Home</NavLi>
+			<NavLi href="/about">About</NavLi>
+			<NavLi href="/services">Services</NavLi>
+			<NavLi href="/pricing">Pricing</NavLi>
+			<NavLi href="/contact">Contact</NavLi>
+		</NavUl>
+	</Navbar>
+</div>
+<div class="container mx-auto px-4">
+	<slot />
+</div>
+
+<div class="container mx-auto px-4">
+	<Footer>
+		<FooterCopyright href="/" by="Feed the Search™" year={2023} />
+		<FooterLinkGroup
+			ulClass="flex flex-wrap items-center mt-3 text-sm text-gray-500 dark:text-gray-400 sm:mt-0"
+		>
+			<FooterLink href="/">About</FooterLink>
+			<FooterLink href="/">Privacy Policy</FooterLink>
+			<FooterLink href="/">Licensing</FooterLink>
+			<FooterLink href="/">Contact</FooterLink>
+		</FooterLinkGroup>
+	</Footer>
+</div>
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,15 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ['./src/**/*.{html,js,svelte,ts}'],
-  theme: {
-    extend: {}
-  },
-  plugins: []
+const config = {
+	content: [
+		'./src/**/*.{html,js,svelte,ts}',
+		'./node_modules/flowbite-svelte/**/*.{html,js,svelte,ts}'
+	],
+
+	theme: {
+		extend: {}
+	},
+
+	plugins: [require('flowbite/plugin')],
+	darkMode: 'class'
 };
+
+module.exports = config;
